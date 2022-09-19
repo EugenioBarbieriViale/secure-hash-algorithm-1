67e326bdce79e0a54406371d671a7d97e257b01c
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2022-09-19 21:48:02 +0200

    new

--- sha1.py
+++ sha1.py
@@ -28,33 +28,25 @@ def random_hex():
 
     return strs
 
+len_in_bin = int_to_bin(len(msg_bin))
+padded = msg_bin + "1" + "0"*(447-len(msg_bin)) + "0"*(64-len(len_in_bin)) + len_in_bin
 
-def pad(msg_bin):
-    temp = []
-    [temp.append("0") for i in range(447-len(msg_bin))]
-    zeros = "".join(temp)
-
-    temp = []
-    len_in_bin = int_to_bin(len(msg_bin))
-    [temp.append("0") for i in range(64-len(len_in_bin))]
-    tail = "".join(temp) + len_in_bin
-
-    return msg_bin + "1" + zeros + tail
-
-arr_words = ["".join([pad(msg_bin)[i] for i in range(j*32,(j+1)*32)]) for j in range(16)]
+arr_words = ["".join([padded[i] for i in range(j*32,(j+1)*32)]) for j in range(16)]
 
 def transform(m):
     for i in range(len(m)):
         m[i] = "0"*48 + m[i]
 
-    print(m)
     arr = []
     for j in range(len(m)):
+        words = []
         for i in range(80):
             if i >= 16 and i <= 79:
-                word_i = int(m[i-3])^int(m[i-8])^int(m[i-14])^int(m[i-16])
-        arr.append(word_i)
+                word_i = int(m[j][i-3])^int(m[j][i-8])^int(m[j][i-14])^int(m[j][i-16])
+                words.append(word_i)
+        print(words)
+        arr.append(words)
     return arr
 
-print(transform(arr_words))
-
+# print(transform(arr_words))
+# transform(arr_words)
