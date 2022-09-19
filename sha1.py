1611de7ed4b386541722022a977673e790479b8b
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2022-09-19 21:28:58 +0200

    new

--- sha1.py
+++ sha1.py
@@ -2,20 +2,16 @@ import random
 
 msg = "abc"
 
-def str_to_bin(msg):
-    return bin(int.from_bytes(str(msg).encode(), "big")).replace("b","")
+def str_to_bin(x):
+    return bin(int.from_bytes(str(x).encode(), "big")).replace("b","")
 
-def int_to_bin(msg):
-    msg = int(msg)
+def int_to_bin(x):
     bin = []
-    while msg >= 2:
-        bin.append(str(msg%2))
-        msg //= 2
-        print(msg, msg%2)
+    while x >= 1:
+        bin.append(str(x%2))
+        x //= 2
 
-    return "".join(bin)
-
-print(int_to_bin(88))
+    return "0"*(8-len(bin)) + "".join(list(reversed(bin)))
 
 msg_bin = str_to_bin(msg)
 
@@ -39,8 +35,26 @@ def pad(msg_bin):
     zeros = "".join(temp)
 
     temp = []
-    len_in_bin = str_to_bin(len(msg_bin))
+    len_in_bin = int_to_bin(len(msg_bin))
     [temp.append("0") for i in range(64-len(len_in_bin))]
     tail = "".join(temp) + len_in_bin
 
     return msg_bin + "1" + zeros + tail
+
+arr_words = ["".join([pad(msg_bin)[i] for i in range(j*32,(j+1)*32)]) for j in range(16)]
+
+def transform(m):
+    for i in range(len(m)):
+        m[i] = "0"*48 + m[i]
+
+    print(m)
+    arr = []
+    for j in range(len(m)):
+        for i in range(80):
+            if i >= 16 and i <= 79:
+                word_i = int(m[i-3])^int(m[i-8])^int(m[i-14])^int(m[i-16])
+        arr.append(word_i)
+    return arr
+
+print(transform(arr_words))
+
