d7d7726075ec91760dbe58295d69857325f30bc3
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2022-09-21 22:06:49 +0200

    new

--- sha1.py
+++ sha1.py
@@ -1,4 +1,5 @@
-import random
+from random import randint
+from numpy import left_shift
 
 msg = "abc"
 
@@ -22,7 +23,7 @@ def random_hex():
     for i in range(5):
         str = []
         for j in range(8):
-            index = random.randint(0,len(chars)-1)
+            index = randint(0,len(chars)-1)
             str.append(chars[index])
         strs.append("".join(str))
 
@@ -44,9 +45,45 @@ def transform(m):
             if i >= 16 and i <= 79:
                 word_i = int(m[j][i-3])^int(m[j][i-8])^int(m[j][i-14])^int(m[j][i-16])
                 words.append(word_i)
-        print(words)
         arr.append(words)
     return arr
 
-# print(transform(arr_words))
-# transform(arr_words)
+A = random_hex()[0]
+B = random_hex()[1]
+C = random_hex()[2]
+D = random_hex()[3]
+E = random_hex()[4]
+
+def f(i, B, C, D):
+    if i <= 19:
+        return (B and C) or ((not B) and D)
+
+    elif i >= 20 and i <= 39:
+        return B ^ C ^ D
+
+    elif i >= 40 and i <= 59:
+        return (B and C) or (B and D) or (C and D)
+
+    elif i >= 60 and i <= 79:
+        return B ^ C ^ D
+
+def K(i):
+    if i <= 19:
+        return "5A827999"
+    
+    elif i >= 20 and i <= 39:
+        return "6ED9EBA1"
+
+    elif i >= 40 and i <= 59:
+        return "8F1BBCDC"
+
+    elif i >= 60 and i <= 79:
+        return "CA62C1D6"
+
+for i in range(80):
+    temp = left_shift(int(str_to_bin(A)), 5) + int(str_to_bin(f(i, B, C, D))) + int(str_to_bin(E)) + int(transform(i)) + int(str_to_bin(k(i)))
+    E = D
+    D = C
+    C = left_shift(B, [30])
+    B = A
+    A = temp
